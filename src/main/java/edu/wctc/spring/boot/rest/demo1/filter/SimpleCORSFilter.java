cbd29d9c21d92729d7ef6f38ba83c513c0c03265
Jim Lombardo <james.g.lombardo@gmail.com>
2014-04-30 13:35:48 -0500

    Added simple CORS (Cross Origin Request Filter) and a Sprng Boot application.yml file to allow different boot configurations

---
+++ src/main/java/edu/wctc/spring/boot/rest/demo1/filter/SimpleCORSFilter.java
@@ -0,0 +1,40 @@
+package edu.wctc.spring.boot.rest.demo1.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+/**
+ * A simple CORS filter to allow all main HTTP methods access to
+ * cross domain web services.
+ * 
+ * @author https://spring.io/guides/gs/rest-service-cors/
+ */
+@Component
+public class SimpleCORSFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
